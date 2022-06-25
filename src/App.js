8b2e8776093cc42634783e489be6aa298f3048d6
MrGiddz <mideolaniyi@outlook.com>
2022-06-26 00:56:43 +0100

    Corrections made to the UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import react from 'react'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Home from './routes/Home'
 import Login from './routes/RPatientLogin'
 import NewPractioner from './components/NewPractioner';
@@ -10,17 +10,17 @@ import PracticeProfile from './components/PracticeProfile';
 function App() {
   return (
     <>
-      <Routes>
-      <Route path='/' element={<Home/>}/>
-      
-      <Route path='/patient/signup' element={<Register/>}/>
-      <Route path='/patient/login' element={<Login/>}/>
-      <Route path='/patient/user' element={<PatientProfile/>}/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          
+          <Route path='/patient/signup' element={<Register/>}/>
+          <Route path='/patient/login' element={<Login/>}/>
+          <Route path='/patient/user' element={<PatientProfile/>}/>
 
-      <Route path='/practice/signup' element={<NewPractioner/>}/>
-      <Route path='/practice/login' element={<PracLogin/>}/>
-      <Route path='/practice/user' element={<PracticeProfile />}/>
-      </Routes>
+          <Route path='/practice/signup' element={<NewPractioner/>}/>
+          <Route path='/practice/login' element={<PracLogin/>}/>
+          <Route path='/practice/user' element={<PracticeProfile />}/>
+        </Routes>
     </>
   );
 }
